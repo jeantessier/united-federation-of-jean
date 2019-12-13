b7b644ee334d90f706137117bd5c23c5697ee07c
Jean Tessier <jean.tessier@gusto.com>
2019-12-12 19:10:03 -0800

    Refactor colliding queries
    The gateway will not allow multiple
    definitions of `Query.book`, or of the type
    `Book` for that matter.

--- service-a/index.js
+++ service-a/index.js
@@ -8,7 +8,7 @@ const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book @key(fields: "title") {
+  type PopularBook @key(fields: "title") {
     title: String!
     author: String
   }
@@ -17,7 +17,7 @@ const typeDefs = gql`
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    popular_books: [PopularBook]
   }
 `;
 
@@ -36,7 +36,7 @@ const books = [
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    popular_books: () => books,
   },
   Book: {
     __resolveReference(book, { fetchBookByTitle }) {
--- service-b/index.js
+++ service-b/index.js
@@ -8,7 +8,7 @@ const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book @key(fields: "title") {
+  type TolkienBook @key(fields: "title") {
     title: String!
     year: Int
   }
@@ -17,7 +17,7 @@ const typeDefs = gql`
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    tolkien_books: [TolkienBook]
   }
 `;
 
@@ -44,7 +44,7 @@ const books = [
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    tolkien_books: () => books,
   },
   Book: {
     __resolveReference(book, { fetchBookByTitle }) {
